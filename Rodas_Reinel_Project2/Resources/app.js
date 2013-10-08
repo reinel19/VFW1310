5f55913e312f5f27b96a31747802a00fddde9a75
Reinel Rodas <reinel.rodas17@gmail.com>
2013-10-08 19:38:18 -0400

    "Project 2"

---
+++ Rodas_Reinel_Project2/Resources/app.js
@@ -0,0 +1,18 @@
+var win = Ti.UI.createWindow();
+
+var navWindow = Ti.UI.createWindow({
+	title: "Best Games Ever Play",
+	modal: true,
+	backgroundColor: "#fff"
+});
+var navGroup = Ti.UI.iPhone.createNavigationGroup({
+	window: navWindow
+});
+
+
+var videoGameFile = require("Games");
+
+
+
+win.add(navGroup);
+win.open();
