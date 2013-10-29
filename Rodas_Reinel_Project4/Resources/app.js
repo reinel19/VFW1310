5fc65bdf2f5384475ef50ebad0e24e4c76292163
Reinel Rodas <reinel.rodas17@gmail.com>
2013-10-29 10:09:50 -0400

    "Final Commit"

--- Rodas_Reinel_Project4/Resources/app.js
+++ Rodas_Reinel_Project4/Resources/app.js
@@ -1,34 +1,75 @@
-var win = Ti.UI.createWindow({
-	title: "Project 4",
-	backgroundColor: "#333",
-	modal: true,
+var win = Ti.UI.createWindow();
+
+
+var navWin = Ti.UI.createWindow({
+	title:"Project4",
+	modal:true,
+	backgroundColor: "#333", 
 	layout: "horizontal"
-
-});
-var newView = Ti.UI.createView({
-	backgroundColor: "#fff",
-	height: 100,
-	top: 10,
-	left: 10,
-	right: 10
-});
-var newLabel = Ti.UI.createLabel({
-	text: "Gallery",
-	font: {fontSize:14,fontFamily:"Helvetica"},
-	color: "#333",
-	left: 20
 });
 
-var openItem = function(){
-	var newWin = Ti.UI.createWindow({
-		title: "Galley",
+var navGroup = Ti.UI.iPhone.createNavigationGroup({
+	window: navWin
+});
+
+// Begining of Gallery Button
+var gPic = function(){
+	var gWin = Ti.UI.createWindow({
+		title: "Gallery",
 		modal: true,
-		backgroundColor: "#000"
+		backgroundColor: "#333",
+		url:"gallery.js"
 	});
+	navGroup.open(gWin,{animation: true});
 };
 
-newView.addEventListener("click", openItem);
 
-newView.add(newLabel);
-win.add(newView);
+// Create a Button.
+var Gallery = Ti.UI.createButton({
+	title : 'Gallery',
+	height : 70,
+	width : 300,
+	top : 10,
+	left : 10
+});
+
+Gallery.addEventListener('click',gPic);
+//end of Gallery button
+
+//Begining of favorite games
+
+var fGames = function(){
+	var fWin = Ti.UI.createWindow({
+		title: "Favorite Games",
+		modal: true,
+		backgroundColor: "#fff",
+		url:"fGamesWin.js"
+	});
+	navGroup.open(fWin,{animation: true});
+};
+
+
+// Create a Button.
+var bButton = Ti.UI.createButton({
+	title : 'Favorite Games',
+	height :70,
+	width : 300,
+	top : 10,
+	left : 10
+});
+
+// Listen for click events.
+bButton.addEventListener('click',fGames);
+//end of favorite games
+
+
+
+
+
+
+navWin.add(Gallery);
+navWin.add(bButton);
+
+win.add(navGroup);
 win.open();
+
